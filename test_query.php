df3afc51809bd8521d58ba968b67d92aa22ad2b1
Mr. Robot <tb.vandiza13@gmail.com>
2026-07-31 23:22:13 +0700

    fix(dashboard): use explicit count loop to avoid withCount select overrides; parse int

---
+++ test_query.php
@@ -0,0 +1,1 @@
+<?php require "vendor/autoload.php"; $app = require_once "bootstrap/app.php"; $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap(); echo json_encode(\Vandiza\NetsightCore\Models\Router::withCount(["pppoeUsersCache as total_count"])->first());
