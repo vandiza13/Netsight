30e678dbf6861071d4f8a391ae8bea69969d7660
Mr. Robot <tb.vandiza13@gmail.com>
2026-08-05 04:38:14 +0700

    fix(acs): add raw fallback parameters for unconfigured GenieACS and remove demo seeds

--- database/seeders/DemoDataSeeder.php
+++ database/seeders/DemoDataSeeder.php
@@ -49,59 +49,6 @@ class DemoDataSeeder extends Seeder
             ]
         );
 
-        // 4. Create Demo ACS TR-069 Devices
-        \Vandiza\NetsightCore\Models\AcsDevice::updateOrCreate(
-            ['genieacs_id' => 'ZTEG-C1234567'],
-            [
-                'serial_number' => 'ZTEGC1234567',
-                'mac_address' => 'CC:2D:E0:11:22:33',
-                'vendor' => 'ZTE',
-                'model' => 'F660 v6.0',
-                'hardware_version' => 'V6.0',
-                'software_version' => 'V6.0.10P1T1',
-                'ip_address' => '10.10.20.45',
-                'rx_power_dbm' => -19.45,
-                'wifi_ssid' => 'NETSIGHT_FIBER_HOME',
-                'pppoe_username' => 'budi_santoso@netsight',
-                'status' => 'online',
-                'last_inform_at' => now(),
-            ]
-        );
 
-        \Vandiza\NetsightCore\Models\AcsDevice::updateOrCreate(
-            ['genieacs_id' => 'HWTC-87654321'],
-            [
-                'serial_number' => 'HWTC87654321',
-                'mac_address' => '00:1E:10:99:88:77',
-                'vendor' => 'Huawei',
-                'model' => 'HG8245H',
-                'hardware_version' => 'V3R015C10',
-                'software_version' => 'V300R015C10SPC130',
-                'ip_address' => '10.10.20.88',
-                'rx_power_dbm' => -26.20,
-                'wifi_ssid' => 'SITI_WIFI_5G',
-                'pppoe_username' => 'siti_rahma@netsight',
-                'status' => 'online',
-                'last_inform_at' => now(),
-            ]
-        );
-
-        \Vandiza\NetsightCore\Models\AcsDevice::updateOrCreate(
-            ['genieacs_id' => 'FHTT-99001122'],
-            [
-                'serial_number' => 'FHTT99001122',
-                'mac_address' => '54:B8:0A:44:55:66',
-                'vendor' => 'FiberHome',
-                'model' => 'HG6245D',
-                'hardware_version' => 'V1.0',
-                'software_version' => 'RP2612',
-                'ip_address' => '10.10.20.102',
-                'rx_power_dbm' => -31.80,
-                'wifi_ssid' => 'AGUS_NET',
-                'pppoe_username' => 'agus_setiawan@netsight',
-                'status' => 'offline',
-                'last_inform_at' => now()->subHours(5),
-            ]
-        );
     }
 }
